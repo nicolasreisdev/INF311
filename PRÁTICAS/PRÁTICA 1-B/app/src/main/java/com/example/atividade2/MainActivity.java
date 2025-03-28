1adacf4054cfcc83fcf761456b228f0f6311f400
Nicolas Reis <nicolas.e.silva@ufv.br>
2025-03-28 16:13:21 -0300

    Adicionando Tela da calculadora.

--- PRÁTICAS/PRÁTICA 1-B/app/src/main/java/com/example/atividade2/MainActivity.java
+++ PRÁTICAS/PRÁTICA 1-B/app/src/main/java/com/example/atividade2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.atividade2;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,52 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    EditText One, Two;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+/*
+    public void acaoBotao(View v){
+        One = (EditText) findViewById(R.id.Valor1Entrada);
+        Two = (EditText) findViewById(R.id.Valor1Entrada);
+        TextView txt = (TextView) findViewById(R.id.Resultado);
+        String tag = v.getTag().toString();
+
+        if(tag.equals("Soma")){
+            double numberOne = Double.parseDouble(One.getText().toString());
+            double numberTwo= Double.parseDouble(One.getText().toString());
+            double sum = (numberOne+numberTwo);
+            String result = String.valueOf(sum);
+            txt.setText(result);
+        } else if (tag.equals("Sub")) {
+            double numberOne = Double.parseDouble(One.getText().toString());
+            double numberTwo= Double.parseDouble(One.getText().toString());
+            double sub = (numberOne-numberTwo);
+            String result = String.valueOf(sub);
+            txt.setText(result);
+        } else if (tag.equals("Mult")) {
+            double numberOne = Double.parseDouble(One.getText().toString());
+            double numberTwo= Double.parseDouble(One.getText().toString());
+            double mult = (numberOne*numberTwo);
+            String result = String.valueOf(mult);
+            txt.setText(result);
+        } else if (tag.equals("Div")) {
+            double numberOne = Double.parseDouble(One.getText().toString());
+            double numberTwo= Double.parseDouble(One.getText().toString());
+            if(numberTwo == 0){
+                txt.setText("Erro!");
+            }
+            else {
+                double div = (numberOne / numberTwo);
+                String result = String.valueOf(div);
+                txt.setText(result);
+            }
+        }
+    }
+    */
 }
