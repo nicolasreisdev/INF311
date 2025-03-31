e84d75d72dfc0ec4594b0d51f90f659f0cf87283
Nicolas Reis <nicolas.e.silva@ufv.br>
2025-03-31 10:13:35 -0300

    Padronizando nome de variáveis.

--- PRÁTICAS/PRÁTICA 1-B/app/src/main/java/com/example/atividade2/MainActivity.java
+++ PRÁTICAS/PRÁTICA 1-B/app/src/main/java/com/example/atividade2/MainActivity.java
@@ -25,116 +25,116 @@ public class MainActivity extends Activity {
         visor = findViewById(R.id.visor);
     }
 
-    public void acaoNumber(View view){
-        String textoAtual = visor.getText().toString();
+    public void numberAction(View view){
+        String currentText = visor.getText().toString();
         String tag = view.getTag().toString();
         if(tag.equals("Zero")){
-            char ultimoCaracter = (textoAtual.length() == 0) ? 'N' : textoAtual.charAt(textoAtual.length()-1);
-            char penultimoCaracter = (textoAtual.length() == 0 || textoAtual.length() == 1) ? 'N' : textoAtual.charAt(textoAtual.length()-2);
-            if(ultimoCaracter == '0' && ultimoCaracter == 'x' || ultimoCaracter == '+' || ultimoCaracter == '-' || ultimoCaracter == '/' || ultimoCaracter == '.' ){
-                visor.setText(textoAtual);
+            char lastCharacter = (currentText.length() == 0) ? 'N' : currentText.charAt(currentText.length()-1);
+            char penultimatemoCaracter = (currentText.length() == 0 || currentText.length() == 1) ? 'N' : currentText.charAt(currentText.length()-2);
+            if(lastCharacter == '0' && lastCharacter == 'x' || lastCharacter == '+' || lastCharacter == '-' || lastCharacter == '/' || lastCharacter == '.' ){
+                visor.setText(currentText);
             }
             else{
-                String newText = textoAtual + "0";
+                String newText = currentText + "0";
                 visor.setText(newText);
             }
         }
         else if(tag.equals("One")){
-            String newText = textoAtual + "1";
+            String newText = currentText + "1";
             visor.setText(newText);
         }
         else if(tag.equals("Two")){
-            String newText = textoAtual + "2";
+            String newText = currentText + "2";
             visor.setText(newText);
         }
         else if(tag.equals("Three")){
-            String newText = textoAtual + "3";
+            String newText = currentText + "3";
             visor.setText(newText);
         }
         else if(tag.equals("Four")){
-            String newText = textoAtual + "4";
+            String newText = currentText + "4";
             visor.setText(newText);
         }
         else if(tag.equals("Five")){
-            String newText = textoAtual + "5";
+            String newText = currentText + "5";
             visor.setText(newText);
         }
         else if(tag.equals("Six")){
-            String newText = textoAtual + "6";
+            String newText = currentText + "6";
             visor.setText(newText);
         }
         else if(tag.equals("Seven")){
-            String newText = textoAtual + "7";
+            String newText = currentText + "7";
             visor.setText(newText);
         }
         else if(tag.equals("Eight")){
-            String newText = textoAtual + "8";
+            String newText = currentText + "8";
             visor.setText(newText);
         }
         else if(tag.equals("Nine")){
-            String newText = textoAtual + "9";
+            String newText = currentText + "9";
             visor.setText(newText);
         }
     }
-    public void acaoAlgebra(View view){
-        String textoAtual = visor.getText().toString();
+    public void operatorAction(View view){
+        String currentText = visor.getText().toString();
         String tag = view.getTag().toString();
         if(tag.equals("Soma")){
-            char ultimoCaracter = (textoAtual.length() == 0) ? 'N' : textoAtual.charAt(textoAtual.length()-1);
-            if(ultimoCaracter == 'x' || ultimoCaracter == '+' || ultimoCaracter == '-' || ultimoCaracter == '/' || ultimoCaracter == '.'){
+            char lastCharacter = (currentText.length() == 0) ? 'N' : currentText.charAt(currentText.length()-1);
+            if(lastCharacter == 'x' || lastCharacter == '+' || lastCharacter == '-' || lastCharacter == '/' || lastCharacter == '.'){
                 return;
             }
             else{
-                String newText = textoAtual + "+";
+                String newText = currentText + "+";
                 visor.setText(newText);
             }
         }
         else if(tag.equals("Sub")) {
-            char ultimoCaracter = (textoAtual.length() == 0) ? 'N' : textoAtual.charAt(textoAtual.length()-1);
-            if(ultimoCaracter == 'x' || ultimoCaracter == '+' || ultimoCaracter == '-' || ultimoCaracter == '/' || ultimoCaracter == '.'){
+            char lastCharacter = (currentText.length() == 0) ? 'N' : currentText.charAt(currentText.length()-1);
+            if(lastCharacter == 'x' || lastCharacter == '+' || lastCharacter == '-' || lastCharacter == '/' || lastCharacter == '.'){
                 return;
             }
             else{
-                String newText = textoAtual + "-";
+                String newText = currentText + "-";
                 visor.setText(newText);
             }
         }
         else if(tag.equals("Div")){
-            char ultimoCaracter = (textoAtual.length() == 0) ? 'N' : textoAtual.charAt(textoAtual.length()-1);
-            if(ultimoCaracter == 'x' || ultimoCaracter == '+' || ultimoCaracter == '-' || ultimoCaracter == '/' || ultimoCaracter == '.'){
+            char lastCharacter = (currentText.length() == 0) ? 'N' : currentText.charAt(currentText.length()-1);
+            if(lastCharacter == 'x' || lastCharacter == '+' || lastCharacter == '-' || lastCharacter == '/' || lastCharacter == '.'){
                 return;
             }
             else{
-                String newText = textoAtual + "/";
+                String newText = currentText + "/";
                 visor.setText(newText);
             }
         }
         else if(tag.equals("Mult")){
-            char ultimoCaracter = (textoAtual.length() == 0) ? 'N' : textoAtual.charAt(textoAtual.length()-1);
-            if(ultimoCaracter == 'x' || ultimoCaracter == '+' || ultimoCaracter == '-' || ultimoCaracter == '/' || ultimoCaracter == '.'){
+            char lastCharacter = (currentText.length() == 0) ? 'N' : currentText.charAt(currentText.length()-1);
+            if(lastCharacter == 'x' || lastCharacter == '+' || lastCharacter == '-' || lastCharacter == '/' || lastCharacter == '.'){
                 return;
             }
             else{
-                String newText = textoAtual + "x";
+                String newText = currentText + "x";
                 visor.setText(newText);
             }
         }
         else if(tag.equals("Ponto")){
-            char ultimoCaracter = (textoAtual.length() == 0) ? 'N' : textoAtual.charAt(textoAtual.length()-1);
-            if(ultimoCaracter == 'x' || ultimoCaracter == '+' || ultimoCaracter == '-' || ultimoCaracter == '/' || ultimoCaracter == '.'){
+            char lastCharacter = (currentText.length() == 0) ? 'N' : currentText.charAt(currentText.length()-1);
+            if(lastCharacter == 'x' || lastCharacter == '+' || lastCharacter == '-' || lastCharacter == '/' || lastCharacter == '.'){
                 return;
             }
             else{
-                String newText = textoAtual + ".";
+                String newText = currentText + ".";
                 visor.setText(newText);
             }
         }
     }
-    public void acaoClear(View view){
-        String textoAtual = visor.getText().toString();
+    public void clearAction(View view){
+        String currentText = visor.getText().toString();
         String tag = view.getTag().toString();
         if(tag.equals("Backspace")){
-            String newText = textoAtual.substring(0, textoAtual.length()-1);
+            String newText = currentText.substring(0, currentText.length()-1);
             visor.setText(newText);
         }
         else if(tag.equals("Limpar")){
@@ -143,47 +143,47 @@ public class MainActivity extends Activity {
         }
     }
     public void Result(View view){
-        String expressao = visor.getText().toString();
-        if(expressao.isEmpty()){
-            visor.setText(expressao);
+        String expression = visor.getText().toString();
+        if(expression.isEmpty()){
+            visor.setText(expression);
         }
         else{
-            expressao = expressao.replace("x", "*");
-            String[] tokens = expressao.split("(?<=[+\\-*/])|(?=[+\\-*/])");
+            expression = expression.replace("x", "*");
+            String[] tokens = expression.split("(?<=[+\\-*/])|(?=[+\\-*/])");
             if (tokens.length % 2 == 0) {
-                String resultado = "Expressão incompleta";
-                visor.setText(resultado);
+                String result = "Expressão incompleta";
+                visor.setText(result);
                 return;
             }
             // Mult and Div
-            double resultado = Double.parseDouble(tokens[0]);
+            double result = Double.parseDouble(tokens[0]);
             for(int i = 1; i < tokens.length;i++){
-                String operador = tokens[i];
+                String operator = tokens[i];
                 double numberTwo = (i == tokens.length-1) ? 0.0 :Double.parseDouble(tokens[i+1]);
-                if(operador.equals("*")){
-                    resultado *= numberTwo;
+                if(operator.equals("*")){
+                    result *= numberTwo;
                 }
-                else if(operador.equals("/")){
+                else if(operator.equals("/")){
                     if(numberTwo == 0){
                         String error = "ERRO! Divisão por zero";
                         visor.setText(error);
                         return;
                     }
-                    resultado /= numberTwo;
+                    result /= numberTwo;
                 }
             }
             // Soma and Sub
             for(int i = 1; i < tokens.length;i++){
-                String operador = tokens[i];
+                String operator = tokens[i];
                 double numberTwo = (i == tokens.length-1) ? 0.0 : Double.parseDouble(tokens[i+1]);
-                if(operador.equals("+")){
-                    resultado += numberTwo;
+                if(operator.equals("+")){
+                    result += numberTwo;
                 }
-                else if(operador.equals("-")){
-                    resultado -= numberTwo;
+                else if(operator.equals("-")){
+                    result -= numberTwo;
                 }
             }
-            visor.setText(String.valueOf(resultado));
+            visor.setText(String.valueOf(result));
         }
 
     }
